d65c537774bc3d45a895a886a2dc9fa00f8492f5
genedeh <genesis.enedeh@gmail.com>
2025-05-25 18:46:13 +0100

    feat: update app name and logo, add SearchBar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold text-dark-200">Hello NativeWind</Text>
+    <View className="flex-1 bg-primary">
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
+        <Image source={images.logo} className="w-12 h-12 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for movies, series, and more"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
