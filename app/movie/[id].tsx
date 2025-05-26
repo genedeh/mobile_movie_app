294e6d6a92f50a4d9c9f19cd9e6a7ba45674010c
genedeh <genesis.enedeh@gmail.com>
2025-05-26 13:46:49 +0100

    feat: improve loading state handling in MovieDetails and enhance movie information display

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -3,7 +3,7 @@ import { fetchMovieDetails } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 
 const MovieInfo = ({ label, value }: { label: string, value?: string | number | null }) => (
@@ -22,50 +22,59 @@ const MovieDetails = () => {
     const { data: movie, loading } = useFetch(() => fetchMovieDetails(id as string));
     console.log('Poster URL:', `https://image.tmdb.org/t/p/w500${movie?.poster_path}`);
     return (
-        <View className="bg-primary flex-1 relative">
-            <ScrollView contentContainerStyle={{ paddingBottom: 100 }} showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={{
-                        uri: movie?.poster_path ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}` : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
-                    }}
-                        style={{ width: '100%', height: 550 }}
-                        resizeMode='stretch'
 
-                    />
-                </View>
-                <View className="flex-col items-start justify-center mt-5 px-5">
-                    <Text className="text-white font-bold text-xl">{movie?.title}</Text>
-                    <View className="flex-row gap-x-1 mt-2 items-center">
-                        <Text className="text-light-200 text-sm">
-                            {movie?.release_date?.split('-')[0] || 'N/A'}
-                        </Text>
-                        <Text className="text-light-200 text-sm">
+        <View className="bg-primary flex-1 relative">
+            {loading ? (
+                <ActivityIndicator
+                    size={"large"}
+                    color="#0000ff"
+                    className="mt-10 self-center"
+                />
+            ) : (
+                <ScrollView contentContainerStyle={{ paddingBottom: 100 }} showsVerticalScrollIndicator={false}>
+                    <View>
+                        <Image source={{
+                            uri: movie?.poster_path ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}` : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                        }}
+                            style={{ width: '100%', height: 550 }}
+                            resizeMode='stretch'
+
+                        />
+                    </View>
+                    <View className="flex-col items-start justify-center mt-5 px-5">
+                        <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+                        <View className="flex-row gap-x-1 mt-2 items-center">
+                            <Text className="text-light-200 text-sm">
+                                {movie?.release_date?.split('-')[0] || 'N/A'}
+                            </Text>
+                            <Text className="text-light-200 text-sm">
+                                {/* @ts-ignore */}
+                                {movie?.runtime}m
+                            </Text>
+                        </View>
+                        <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+                            <Image source={icons.star} className="size-4" />
+                            <Text className="text-white font-bold text-sm">
+                                {Math.round(movie?.vote_average ?? 0) || 'N/A'}/10
+                            </Text>
+                            <Text className="text-light-200 text sm">
+                                ({movie?.vote_count} votes)
+                            </Text>
+                        </View>
+                        <MovieInfo label="Overview" value={movie?.overview} />
+                        {/* @ts-ignore */}
+                        <MovieInfo label="Genres" value={movie?.genres?.map((g) => g.name).join(' - ') || 'N/A'} />
+                        <View className="flex flex-row justify-between w-1/2">
                             {/* @ts-ignore */}
-                            {movie?.runtime}m
-                        </Text>
-                    </View>
-                    <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
-                        <Image source={icons.star} className="size-4" />
-                        <Text className="text-white font-bold text-sm">
-                            {Math.round(movie?.vote_average ?? 0) || 'N/A'}/10
-                        </Text>
-                        <Text className="text-light-200 text sm">
-                            ({movie?.vote_count} votes)
-                        </Text>
-                    </View>
-                    <MovieInfo label="Overview" value={movie?.overview} />
-                    {/* @ts-ignore */}
-                    <MovieInfo label="Genres" value={movie?.genres?.map((g) => g.name).join(' - ') || 'N/A'} />
-                    <View className="flex flex-row justify-between w-1/2">
+                            <MovieInfo label="Budget" value={`$${movie?.budget / 1_000_000} millions `} />
+                            {/* @ts-ignore */}
+                            <MovieInfo label="Revenue" value={`$${Math.round(movie?.revenue) / 1_000_000} millions `} />
+                        </View>
                         {/* @ts-ignore */}
-                        <MovieInfo label="Budget" value={`$${movie?.budget / 1_000_000} millions `} />
-                        {/* @ts-ignore */}
-                        <MovieInfo label="Revenue" value={`$${Math.round(movie?.revenue) / 1_000_000} millions `} />
+                        <MovieInfo label="Production Companies" value={movie?.production_companies?.map((c) => c.name).join(' - ') || 'N/A'} />
                     </View>
-                    {/* @ts-ignore */}
-                    <MovieInfo label="Production Companies" value={movie?.production_companies?.map((c) => c.name).join(' - ') || 'N/A'} />
-                </View>
-            </ScrollView>
+                </ScrollView>
+            )}
             <View className="absolute bottom-6 left-0 right-0 items-center z-50">
                 <TouchableOpacity
                     onPress={router.back}
