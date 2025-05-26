da6be014cdbc21d8c4e068badf9c21feba38e804
genedeh <genesis.enedeh@gmail.com>
2025-05-26 11:21:50 +0100

    feat: add search count tracking functionality and integrate Appwrite service

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -2,6 +2,7 @@ import MovieCard from '@/components/MovieCard';
 import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
 import useFetch from '@/services/useFetch';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
@@ -15,6 +16,9 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+                if (movies?.length > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
                 reset();
             }
